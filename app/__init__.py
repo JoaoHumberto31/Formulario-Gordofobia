7d4831347b9feae64fed46dd25db49877a1c4443
Joao Humberto Tecila Mayer <joao_h_mayer@estudante.sesisenai.org.br>
2025-10-01 14:27:04 -0300

    .env

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 import os
 
+load_dotenv('.env')
 # Inicializa as extensões (sem anexar à app ainda)
 db = SQLAlchemy()
 migrate = Migrate()
@@ -13,8 +15,8 @@ def create_app():
     app = Flask(__name__)
     
     # Configurações da Aplicação
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'chave_secreta_muito_segura_aqui')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///respostas.db'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
